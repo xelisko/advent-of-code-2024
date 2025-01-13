2ccab1d46d4d38fd435c2ff0e7fe62e9390979f2
Alex Geciova <ageciova@gmail.com>
2025-01-13 16:13:22 +0800

    read file
    open and read text file

---
+++ 01_day.py
@@ -0,0 +1,14 @@
+import math
+import numpy
+
+# - list of locations
+# - location id
+# - pair up and how far they are
+# - pair the smallest numbers
+
+# read the text file
+file = open('input.txt', 'r')
+inputs = file.read()
+print (inputs)
+file.close()
+#
