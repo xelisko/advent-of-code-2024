17fa9049ae6fce7c90cbc0d7db33b6c780cdd927
Alex Geciova <ageciova@gmail.com>
2025-01-13 16:39:21 +0800

    implementation
    implementation of day 1

--- 01_day.py
+++ 01_day.py
@@ -1,14 +1,42 @@
 import math
-import numpy
+import numpy as np
 
-# - list of locations
-# - location id
-# - pair up and how far they are
-# - pair the smallest numbers
+# define lists
+list1 = []
+list2 = []
+distances = []
+similarity_score = []
 
 # read the text file
-file = open('input.txt', 'r')
-inputs = file.read()
-print (inputs)
+file = open('input01.txt', 'r')
+line = file.readline().strip()
+while line:
+    a, b = line.split('   ')
+    list1.append(int(a))
+    list2.append(int(b))
+    line = file.readline().strip()
+
 file.close()
-#
+
+# sort the list
+list1.sort()
+list2.sort()
+
+# calculate the distance
+for i in range (len(list1)):
+    d = np.abs(list1[i] - list2[i])
+    # print (i, list1[i], list2[i], d)
+    distances.append(d)
+
+# print the total distance
+print ("Total distance = ", sum(distances))
+
+# part B - Similarity Score
+for i in range (len(list1)):
+    x = list1[i]
+    n_appear = list2.count(x)
+    score = x * n_appear
+    # print (i, score)
+    similarity_score.append(score)
+
+print ("Similarity Score = ", sum(similarity_score))
