57dcab914ae0add0db36e11f11395d02b5702415
Alex Geciova <ageciova@gmail.com>
2025-01-15 09:19:27 +0800

    day 3 finalised

--- 03_day.py
+++ 03_day.py
@@ -1,68 +1,101 @@
+import re
+
 def main ():
-
-    # define lists
-    sum = 0 
-
+    sum = 0
+    sum_part1 = 0 
+    prev_args = ""
+    activate = True
+    first_time = True
+    
     # read the text file
     file = open('input2.txt', 'r')
+    line = file.readline()
 
-    line = file.readline()#.strip()
     while line:
-        print (line)
-        # take arguments of every 'mul'
-        line.find(r
+        first_time = True
+        index = line.find('mul')
 
-        # tokens = line.split('mul', 'don't()', 'do()')
-        print (tokens)
+        while index != -1:
 
-        # filter out the ones that do not start with '('
-        tokens = noFirstBracket(tokens)
-        print(tokens)
- 
-        # try to extract A and B
-        sum += extractNumbersandMultiply(tokens)
-        print ("sum = ", sum)
+            # select until the next 'mul'
+            next_index = line.find('mul', index + 1)
+            args = line[index + 3: next_index]
 
-        line = file.readline()#.strip()
+            # check for validity
+            valid = correctFormat(args)
+
+            if (not valid):
+                index = next_index
+                prev_args = prev_args + args
+                continue   
+            
+            # calculate the product
+            product = extractNumbersandMultiply(args)
+            sum_part1 += product
+            
+            # search de/activation
+            if (first_time):
+                first_time = False
+                activate = True
+                prev_args = prev_args.strip() + line[:index]
+            
+            activate = activation(prev_args, activate)
+            if (activate):
+                sum += product
+
+            prev_args = args
+            if (next_index == -1):
+                prev_args = line[index+3:]
+            index = next_index
+
+        line = file.readline()
     
+    print ("final sums = ", sum_part1, sum)
     file.close()
     return 0
 
-def noFirstBracket(tokens):
-    output = []
-    for token in tokens:
-        if len(token) == 0:
-            continue
-        if ((token[0] is '(') and ( ',' in token) and (')' in token)):
-            output.append(token)
-    return output
+def activation(args, active_prev):
+    # reverse the string 
+    active = True
+    dont_index = args.rfind("don't()")
+    do_index = args.rfind("do()")
+    
+    if (dont_index > do_index):
+        active = False
+    elif (dont_index < do_index):
+        active = True
+    else:
+        active = active_prev
+    return active
 
-def extractNumbersandMultiply(tokens):
+def correctFormat(args):
+    # valid = True
+    if len(args) == 0:
+        return False
+    if ((args[0] == '(') and ( ',' in args) and (')' in args)):
+        return True
+    return False
+    
+
+def extractNumbersandMultiply(token):
     sum = 0
-    for token in tokens:
-        print (token)
-        comma = token.find(',')
-        a = token[1:comma]
-        # print ("A = ", a)
-        closingBracket = token.find(')')
-        b = token[comma+1:closingBracket]
-        # print ("B = ", b)
+    
+    comma = token.find(',')
+    a = token[1:comma]
+    closingBracket = token.find(')')
+    b = token[comma+1:closingBracket]
 
-        # only digits
-        if (a.isdigit() == False or b.isdigit() == False):
-            # print ("A or B is not a valif INTEGER")
-            continue
-        # test the integer validity
-        try:
-            a = int(a)
-            b = int(b)
-        except ValueError:
-        #    print ("A or B not an int") 
-           continue
-        
-        if ((a < 1000) and (b <1000)):
-            print ("OK", a, b, a*b)
-            sum += a*b
+    # only digits
+    if (a.isdigit() == False or b.isdigit() == False):
+        return 0
+    
+    # test the integer validity
+    a = int(a)
+    b = int(b)
+    
+    if ((a < 1000) and (b <1000)):
+        sum += a*b
+
     return sum
         
 
