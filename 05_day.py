0c0c13906e7e006f3cd86310d0d07d7092201850
Alex Geciova <ageciova@gmail.com>
2025-02-15 10:21:16 +0800

    changes

--- 05_day.py
+++ 05_day.py
@@ -1,20 +1,17 @@
-
-
 def main():
-
-    file = open ("05_puzzle.txt", "r")
+    file = open("05_puzzle.txt", "r")
     line = file.readline().strip()
 
     rule_dict = dict()
 
     # read all the rules and save to dictionary
     while line != "":
-        line1 = line.split('|')
-        x, y = int(line1[0]), int(line1[1]) 
-        
-        if (rule_dict.get(x) != None):
+        line1 = line.split("|")
+        x, y = int(line1[0]), int(line1[1])
+
+        if rule_dict.get(x) != None:
             existing = rule_dict.get(x)
-            if (isinstance(existing, int)):
+            if isinstance(existing, int):
                 rule_dict[x] = [existing, y]
             else:
                 rule_dict[x] = existing + [y]
@@ -28,13 +25,13 @@ def main():
     sum = 0
     incorrect_orders = []
     while line:
-        tokens = list(map(int, line.split(',')))
-        
+        tokens = list(map(int, line.split(",")))
+
         valid = checkValidity(tokens, rule_dict)
 
         # add up the middle value
-        if (valid):
-            middle_index = len(tokens)//2
+        if valid:
+            middle_index = len(tokens) // 2
             middle_value = tokens[middle_index]
             sum += middle_value
         else:
@@ -42,79 +39,80 @@ def main():
 
         line = file.readline().strip()
 
-    print ("sum part 1 = ", sum)
-    
+    print("sum part 1 =", sum)
+
     sum1 = 0
     for order in incorrect_orders:
         correct = correctOrder(order, rule_dict)
-        sum1 += correct[len(correct)//2]
-    
-    print ("sum part 2 = ", sum1)
+        sum1 += correct[len(correct) // 2]
+
+    print("sum part 2 =", sum1)
     return 0
+
 
 def checkValidity(tokens, rules_dict):
     prev_tokens = []
 
     for token in tokens:
-        if (len(prev_tokens)==0):
+        if not prev_tokens:
             prev_tokens.append(token)
             continue
 
-        # get the dictionary entry
         cannot_be_before = rules_dict.get(token)
-    
-        # if only 0 or 1 rule
-        if (cannot_be_before==None):
+
+        if not cannot_be_before:
             prev_tokens.append(token)
             continue
-        if (isinstance(cannot_be_before, int)):
-            if (cannot_be_before in prev_tokens):
+
+        if isinstance(cannot_be_before, int):
+            if cannot_be_before in prev_tokens:
                 return False
             prev_tokens.append(token)
             continue
-        
-        # loop through 
+
         for page in prev_tokens:
-            if (page in cannot_be_before):
+            if page in cannot_be_before:
                 return False
 
         prev_tokens.append(token)
     return True
+
 
 def correctOrder(order, rules_dict):
     prev_ids = []
 
     # let's swap the ones that are in an incorrect order
     for id in order:
-        prev_ids.append(id) 
+        prev_ids.append(id)
 
         # get the dictionary entry
         no_go_ids = rules_dict.get(id)
-    
-        if (len(prev_ids)==0):
+
+        if len(prev_ids) == 0:
             continue
 
         # if no rules
-        if (no_go_ids == None):
+        if no_go_ids == None:
             continue
 
         # if only 1 rule -> convert int to list
-        if (isinstance(no_go_ids, int)):
+        if isinstance(no_go_ids, int):
             no_go_ids = [no_go_ids]
-        
+
         # loop through
-        idx_id = 0 
+        j = 0
         # reverse the array, so the checking happens from the end
-        prev_ids = prev_ids [::-1]
+        prev_ids = prev_ids[::-1]
 
         for i, prev in enumerate(prev_ids):
-            if (prev in no_go_ids):
-                prev_ids[i], prev_ids[idx_id] = prev_ids[idx_id], prev_ids[i]
-                idx_id = i
+            if prev in no_go_ids:
+                prev_ids[i], prev_ids[j] = prev_ids[j], prev_ids[i]
+                j = i
         # reverse back
         prev_ids = prev_ids[::-1]
 
     return prev_ids
+
 
 if __name__ == "__main__":
     main()
--- 06_day.py
+++ 06_day.py
@@ -1,79 +1,78 @@
-
 class Map:
     def __init__(self, grid):
         self.grid = grid
         self.rows = len(grid)
         self.cols = len(grid[0])
-    
+
     def isObstable(self, position):
         r = position[0]
         c = position[1]
 
-        if (self.grid[r][c] == "#"):
+        if self.grid[r][c] == "#":
             return True
         return False
-    
+
     def getMap(self):
         for row in self.grid:
-            print (row)
-    
+            print(row)
+
     def getStartPos(self):
         start_r, start_c = -1, -1
-        for r,row in enumerate(self.grid):
-            if ("^" in row):
+        for r, row in enumerate(self.grid):
+            if "^" in row:
                 start_c = row.find("^")
                 start_r = r
                 continue
-        if (start_r == -1 or start_c == -1):
-            print ("Error: initial position not found")
-        return [start_r, start_c] 
-    
+        if start_r == -1 or start_c == -1:
+            print("Error: initial position not found")
+        return [start_r, start_c]
+
     def markVisit(self, pos):
         r = pos[0]
         c = pos[1]
 
-        self.grid[r] = self.grid[r][:c]+"X"+self.grid[r][c+1:]
+        self.grid[r] = self.grid[r][:c] + "X" + self.grid[r][c + 1 :]
         # print (self.grid[r])
 
     def move(self, dir, pos):
         r = pos[0]
         c = pos[1]
 
-        match(dir):
-            case 1: # right
-                return [r, c+1]
-            case 3: # left
-                return [r, c-1]
-            case 0: # up
-                return [r-1, c]
-            case 2: # down
-                return [r+1, c]
+        match (dir):
+            case 1:  # right
+                return [r, c + 1]
+            case 3:  # left
+                return [r, c - 1]
+            case 0:  # up
+                return [r - 1, c]
+            case 2:  # down
+                return [r + 1, c]
             case default:
-                return [r, c+1]
-    
+                return [r, c + 1]
+
     def isInsideMap(self, pos):
         r = pos[0]
         c = pos[1]
-        if (r <= 0 or c <= 0 or r >= self.rows-1 or c >= self.cols-1):
+        if r <= 0 or c <= 0 or r >= self.rows - 1 or c >= self.cols - 1:
             return False
         return True
-    
+
     def countVisited(self):
         vis = 0
-        for j in range (self.rows):
-            for i in range (self.cols):
-                if (self.grid[j][i] == "X"):
+        for j in range(self.rows):
+            for i in range(self.cols):
+                if self.grid[j][i] == "X":
                     vis += 1
         return vis
 
     def changeDir(self, curr_dir):
         new_dir = curr_dir + 1
-        if (new_dir == 4):
+        if new_dir == 4:
             return 0
         return new_dir
 
 
-def main ():
+def main():
     f = open("06_puzzle.txt", "r")
     line = f.readline().strip()
 
@@ -83,13 +82,13 @@ def main ():
     while line:
         map_read.append(line)
         line = f.readline().strip()
-    
+
     # initiate the map object
     map = Map(map_read)
     pos = map.getStartPos()
-    dir = 0 #"UP"
+    dir = 0  # "UP"
 
-    print (dir, pos)
+    print(dir, pos)
 
     # start moving
     inside = True
@@ -103,12 +102,12 @@ def main ():
         obstacle = map.isObstable(next_pos)
         # print (obstacle)
 
-        if (obstacle):
+        if obstacle:
             # change direction
             dir = map.changeDir(dir)
             # get new position
             next_pos = map.move(dir, pos)
-        
+
         # if no obstacle, move there
         map.markVisit(next_pos)
         pos = next_pos
@@ -119,7 +118,7 @@ def main ():
 
     # escaped map -> # of visited tiles
     number = map.countVisited()
-    print ("# of visited tiles", number)
+    print("# of visited tiles", number)
     return 0
 
 
