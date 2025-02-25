fb619f43b12049c88b954ecaf76030997e15fd53
Alex Geciova <ageciova@gmail.com>
2025-02-25 23:40:35 +0800

    vebessern tag7

--- 07_day.py
+++ 07_day.py
@@ -1,65 +1,60 @@
 import numpy as np
 import time
 
+
 def main():
     start = time.time()
 
-    f = open("07_puzzle.txt", "r")
-    line = f.readline().strip()
     sum = 0
+    with open("07_input.txt") as f:
+        for line in f:
+            # read and process the line
+            values = line.strip().split(" ")
+            result = int(values[0][:-1])
+            values = list(map(int, values[1:]))
 
-    while line:
-        # read and process the line
-        values = line.split(' ')
-        result = int(values[0][:-1])
-        values = list(map(int, values[1:]))
+            # get all the combinations of operators
+            all_equations = np.array(
+                generate_operators_combinations("", len(values) - 1)
+            )
+            all_equations = all_equations.reshape((-1))
+            # print(all_equations)
+            # print(len(all_equations), len(values))
 
-        # get all the combinations of operators
-        operators = np.array(generateRecursive('', len(values)-1))
-        operators = operators.reshape((-1))
+            # calculate all the values
+            all_results = []
+            for operators_in_equation in all_equations:
+                intermediate = values[0]
+                for i, operator in enumerate(operators_in_equation):
+                    if operator == "+":
+                        intermediate += values[i + 1]
+                    elif operator == "*":
+                        intermediate *= values[i + 1]
+                    elif operator == "|":
+                        intermediate = int(str(intermediate) + str(values[i + 1]))
 
-        # calculate all the values
-        all_results = []
-        for comb in operators: # pick a combination
-            intrmdt = values[0] # set the first values
+                all_results.append(intermediate)
 
-            for i,opr in enumerate(comb): # for each operator in the list
-                if (opr == '+'):
-                    intrmdt += values[i+1]
-                elif (opr == '*'):
-                    intrmdt *= values[i+1]
-                elif (opr == "|"):
-                    intrmdt = int(str(intrmdt)+str(values[i+1]))
-                # print(opr, intrmdt)
-            # print (comb)
+            if result in all_results:
+                sum += result
 
-            all_results.append(intrmdt)
-
-        if (result in all_results):
-            sum += result 
-        
-        line = f.readline().strip()
-        
-    print ("sum of the correct equations = ", sum)
+    print("sum of the correct equations = ", sum)
     end = time.time()
-    print ("processing time = ", end - start)
+    print("processing time = ", end - start)
     return 0
 
-def generateRecursive(curr_str, n):
-    if (len(curr_str)==n):
+
+def generate_operators_combinations(curr_str: str, n: int) -> list[str]:
+    if len(curr_str) == n:
         return str(curr_str)
-    
-    # recursive call
+
     combinations = []
-    combinations.append(generateRecursive(curr_str+'*', n))
-    combinations.append(generateRecursive(curr_str+'+', n))
-    combinations.append(generateRecursive(curr_str+'|', n))
+    combinations.append(generate_operators_combinations(curr_str + "*", n))
+    combinations.append(generate_operators_combinations(curr_str + "+", n))
+    combinations.append(generate_operators_combinations(curr_str + "|", n))
 
     return combinations
 
+
 if __name__ == "__main__":
     main()
-    # a = generateRecursive('', 5)
-    # b = np.array(a)
-    # b = b.reshape((-1))
-    # print (b)
